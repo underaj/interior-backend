9f71c08bfe6a8bf0570a201c2a3e3128a97ff9f6
ching yang <chingyang91@gmail.com>
2022-02-10 21:13:07 +0800

    Fix

--- services/db.js
+++ services/db.js
@@ -3,7 +3,7 @@ const conn = mysql.createConnection({
  host: "rm-wz9k4a0a001y93vxf.mysql.rds.aliyuncs.com",
  user: "underaj",
  password: "ching123!",
- database: "interior-design",
+ database: "interior_design",
 });
 
 conn.connect();
