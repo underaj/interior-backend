ba39d652b80778c47f8b909c9981fe283a34698a
ching yang <chingyang91@gmail.com>
2022-02-13 19:49:51 +0800

    Delete product controller and route

--- controllers/index.js
+++ controllers/index.js
@@ -16,16 +16,35 @@ exports.getAllProducts = (req, res, next) => {
  exports.createProduct = (req, res, next) => {
   if (!req.body) return next(new AppError("No form data found", 404));
   const data = req.body;
-  const values = [data.name, data.type, format(new Date(), 'yyyy-MM-dd hh:mm:ss'), data.companyName, data.mediaUrl];
+  const currentDate = new Date();
+  const productId = `${format(currentDate, 'yyyyMMdd')}${String(Date.now())}}`
+  const values = [productId, data.name, data.modelNumber, data.type, data.category, data.price, data.supplierName, data.mediaUrl, format(currentDate, 'yyyy-MM-dd hh:mm:ss'), format(currentDate, 'yyyy-MM-dd hh:mm:ss')];
   conn.query(
-    "INSERT INTO product (name, type, register_date,company_name, media_url) VALUES(?)",
+    "INSERT INTO product (id, name, model_no, type, category, price, supplier_name, media_url, register_time, update_time) VALUES(?)",
     [values],
     function (err, data, fields) {
       if (err) return next(new AppError(err, 500));
       res.status(201).json({
         status: "success",
-        message: "Product created!",
+        message: "Product created",
       });
     }
   );
  };
+
+ exports.deleteProduct = (req, res, next) => {
+  if (!req.params.id) {
+    return next(new AppError("No product found", 404));
+  }
+  conn.query(
+    "DELETE FROM product WHERE id=?",
+    [req.params.id],
+    function (err, fields) {
+      if (err) return next(new AppError(err, 500));
+      res.status(201).json({
+        status: "success",
+        message: "Product deleted",
+      });
+    }
+  );
+ }
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,5 @@ const controllers = require("../controllers");
 const router = express.Router();
 
 router.route("/product").get(controllers.getAllProducts).post(controllers.createProduct);
+router.route("/product/:id").put(controllers.updateProduct).delete(controllers.deleteProduct);
 module.exports = router;
